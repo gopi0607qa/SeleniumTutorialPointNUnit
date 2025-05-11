ea04d00c0a3817a6bd2ec32aa834edc5b8cc02f6
Gopinath K <gopiqa0607@gmail.com>
2025-05-11 19:35:49 +0530

    Added ThreadLocal for parelle execution

--- SeleniumTutorialPointNUnit/UnitTest1.cs
+++ SeleniumTutorialPointNUnit/UnitTest1.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
 
@@ -8,18 +9,42 @@ namespace SeleniumTutorialPointNUnit
 {
     public class Tests
     {
-        private IWebDriver driver;
-        private string Url = "https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php";
+
+        ThreadLocal<IWebDriver> driver = new();
+        private string HomeUrl = "https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php";
+        private string LoginUrl = "https://www.tutorialspoint.com/selenium/practice/login.php";
+
         private string homePageTitlexpath = "//form[@id='practiceForm']/h1";
+
+        private string loginPageTitlexpath = "//form[@id='signInForm']/h1";
+
         [SetUp]
         public void Setup()
         {
-            driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
+            driver.Value = DriverInvocation("chrome");
+
 
         }
 
-        private void WaitforElementVisible(IWebDriver driver ,string xpath) {
+        private static IWebDriver DriverInvocation(string browsername) {
+
+            IWebDriver createdriverinstance = null;
+            switch (browsername) {
+                case "chrome":
+                    createdriverinstance = new ChromeDriver();
+                    createdriverinstance.Manage().Window.Maximize();
+                    break;
+                case "edge":
+                    createdriverinstance = new EdgeDriver();
+                    createdriverinstance.Manage().Window.Maximize();
+
+                    break;
+                    
+            }
+            return createdriverinstance;
+        }
+
+        private static void WaitforElementVisible(IWebDriver driver ,string xpath) {
 
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
 
@@ -28,23 +53,53 @@ namespace SeleniumTutorialPointNUnit
         
         }
 
-        [Test]
+        [Test, Parallelizable]
         public void HomepagetitleCheck()
         {
-            driver.Navigate().GoToUrl(Url);
 
-            WaitforElementVisible(driver, homePageTitlexpath);
+            IWebDriver currentdriverinstance = driver.Value;
 
-            string homeTitle = driver.FindElement(By.XPath(homePageTitlexpath)).Text;
+            currentdriverinstance.Navigate().GoToUrl(HomeUrl);
+
+            WaitforElementVisible(currentdriverinstance, homePageTitlexpath);
+
+            string homeTitle = currentdriverinstance.FindElement(By.XPath(homePageTitlexpath)).Text;
 
             Assert.That(homeTitle, Is.EqualTo("Student Registration Form"));
+
+            Thread.Sleep(5000);
+
+
+        }
+
+        [Test, Parallelizable]
+        public void LoginpagetitleCheck()
+        {
+            IWebDriver currentdriverinstance = driver.Value;
+
+            currentdriverinstance.Navigate().GoToUrl(LoginUrl);
+
+            WaitforElementVisible(currentdriverinstance, loginPageTitlexpath);
+
+            string homeTitle = currentdriverinstance.FindElement(By.XPath(loginPageTitlexpath)).Text;
+
+            Assert.That(homeTitle, Is.EqualTo("Welcome, Login In"));
+
+            Thread.Sleep(5000);
+
+
+
         }
 
         [TearDown]
 
         public void TearDown() {
 
-            driver.Dispose();
+            Console.WriteLine(driver.ToString());
+
+            driver.Value.Quit();
+            driver.Value.Dispose();
+
         }
     }
 }
